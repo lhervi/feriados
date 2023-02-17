b58dcdaab3d4f24de99238b7ad9ec6c7bb8abf86
Lornis Hervilla <lhervi@gmail.com>
2023-02-17 08:53:08 -0400

    first commit

---
+++ getFeriado.php
@@ -0,0 +1,37 @@
+<?php
+
+$ahora=getdate();
+
+$meses = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'];
+$dias = ['domingo', 'lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado'];
+
+
+if(isset($_GET['año'])){
+    $año = intval($_GET['año']);    
+}else{
+    $año = $ahora['year'];    
+}
+
+if(isset($_GET['mes'])){    
+    $mes = intval($_GET['mes']);  
+}
+
+if(isset($_GET['dia'])){
+    $mes = intval($_GET['mes']);  
+}
+
+
+$file = file_get_contents("feriados.json");
+$fileArray = json_decode($file, true);
+
+$respuesta = null;
+
+if (isset($fileArray[$año])){
+    $respuesta = $fileArray[$año];
+    if(isset($fileArray[$año][$meses[$mes-1]])){
+        $respuesta = $fileArray[$año][$meses[$mes-1]];
+    }
+}
+
+echo json_encode($respuesta);
+?>
